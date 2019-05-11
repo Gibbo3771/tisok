e1e3f6c22ba4be7275433822e9cbc16362d0ed19
Stephen Gibson <gibson.stephen92@gmail.com>
2019-05-11 01:42:28 +0100

    added media queries, now responsive for <480px and <568px screens, as well as ipad/pixel ready

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default class App extends React.Component {
   render() {
     const { advice } = this.state;
     return (
-      <div className="flex">
+      <div className="grid">
         <AdviceSlip advice={advice} />
         <AdviceButton onClick={this.handleClick} />
       </div>
