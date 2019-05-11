3b29ffbac52530f36d43e5abf60a62e567e2ce2c
Stephen Gibson <gibson.stephen92@gmail.com>
2019-05-11 01:05:35 +0100

    fixed button shadow, gave window a title. Still needs favicon

---
+++ build/precache-manifest.62a1bb0d263d09ae9aba87903fb4e4e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0b163f46edc4ef431bc2f6a1c8fc1ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffeae0547a2abe68586d",
+    "url": "/static/css/main.28669424.chunk.css"
+  },
+  {
+    "revision": "192b264d476ff488a2db",
+    "url": "/static/js/2.dc7c8abb.chunk.js"
+  },
+  {
+    "revision": "ffeae0547a2abe68586d",
+    "url": "/static/js/main.53cf3a9a.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  }
+]);
