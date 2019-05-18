844eba5617c971f68271177b70cfc088dd6c3046
user <gibson.stephen92@gmail.com>
2019-05-18 15:46:26 +0100

    added pose animations to text

--- src/componets/AdviceSlip/AdviceSlip.jsx
+++ src/componets/AdviceSlip/AdviceSlip.jsx
@@ -2,22 +2,55 @@
 import React from "react";
 import { Slip } from "../PropTypes";
 import Anime from "react-anime";
-import posed from "react-pose";
+import posed, { PoseGroup } from "react-pose";
+import SplitText from "react-pose-text";
 
 export type Props = {
   slip: Slip
 };
 
+const charPoses = {
+  exit: {
+    opacity: 0,
+    y: 80,
+    transition: ({ charInWordIndex }) => ({
+      type: "spring",
+      delay: charInWordIndex * 10,
+      stiffness: 200 + charInWordIndex * 90,
+      damping: 8
+    })
+  },
+  enter: {
+    opacity: 1,
+    y: 0,
+    transition: ({ charInWordIndex }) => ({
+      type: "spring",
+      delay: charInWordIndex * 10,
+      stiffness: 200 + charInWordIndex * 90,
+      damping: 8
+    })
+  }
+};
+
+const Container = posed.div({
+  enter: { opacity: 1, delay: 0, beforeChildren: true },
+  exit: { opacity: 0 }
+});
+
 export default class AdviceSlip extends React.Component<Props> {
   render() {
     const { slip } = this.props;
+    console.log("render");
+
     if (!slip.advice) return <div />;
     return (
-      <div id="advice-text" className="advice-text">
-        {slip.advice.split("").map((v, i) => (
-          <span key={i}>{v}</span>
-        ))}
-      </div>
+      <PoseGroup id="advice-text" className="advice-text">
+        <Container key={slip.advice}>
+          <SplitText initialPose="exit" pose="enter" charPoses={charPoses}>
+            {slip.advice}
+          </SplitText>
+        </Container>
+      </PoseGroup>
     );
   }
 }
