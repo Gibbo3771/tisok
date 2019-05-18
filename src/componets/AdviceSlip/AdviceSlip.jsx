dc843589e3863566a990cc64424779be598fd14c
user <gibson.stephen92@gmail.com>
2019-05-18 14:42:13 +0100

    added react pose dependency

--- src/componets/AdviceSlip/AdviceSlip.jsx
+++ src/componets/AdviceSlip/AdviceSlip.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Slip } from "../PropTypes";
 import Anime from "react-anime";
+import posed from "react-pose";
 
 export type Props = {
   slip: Slip
@@ -11,10 +12,6 @@ export default class AdviceSlip extends React.Component<Props> {
   render() {
     const { slip } = this.props;
     if (!slip.advice) return <div />;
-    const animeProps = {
-      opacity: [0, 1],
-      delay: (element, index) => index * 15
-    };
     return (
       <div id="advice-text" className="advice-text">
         {slip.advice.split("").map((v, i) => (
